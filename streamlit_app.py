fcc6617274e850553eb7069a3ddec990c5da27ef
Grace Leung Shing <gs.leungshing@gmail.com>
2023-08-31 18:00:51 +0400

    minor fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from st_files_connection import FilesConnection
 # Create connection object and retrieve file contents.
 # Specify input format is a csv and to cache the result for 600 seconds.
 conn = st.experimental_connection('gcs', type=FilesConnection)
-df = conn.read("streamlit-bucket/myfile.csv", input_format="csv", ttl=600)
+df = conn.read("streamlit-bucket/myfile.csv", input_format="csv")
 
 # Print results.
 for row in df.itertuples():
